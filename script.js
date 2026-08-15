7d12855554f19263e921109b5c4e9efeb970ab2f
Lainey Rivers <laineyrivers@Laineys-MBP.attlocal.net>
2026-08-15 00:42:13 -0400

    Add Animations to portfolio content, Add profile photo

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ themeToggle.addEventListener("click", () => {
         themeToggle.textContent = "🌙";
     }
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+});
